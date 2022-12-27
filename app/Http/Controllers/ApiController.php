3e3df3e8125bc3fa5a356017a48063460cad5c14
aditya <mac-air@MAC-AIRs-MacBook-Air.local>
2022-12-27 17:04:10 +0700

    init

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Signifly\Shopify\Shopify;
+
+class ApiController extends Controller
+{
+    public function getProductCount()
+    {
+        $shopify = new Shopify(
+            env('SHOPIFY_ACCESS_TOKEN'),
+            env('SHOPIFY_DOMAIN'),
+            env('SHOPIFY_API_VERSION')
+        );
+
+        $response = $shopify->getProductsCount();
+
+        if ($response) {
+            return [
+                'total_pages'  => $response,
+                'status' => true
+            ];
+        }
+
+        return false;
+    }
+
+    private function getProducts()
+    {
+        $shopify = new Shopify(
+            env('SHOPIFY_ACCESS_TOKEN'),
+            env('SHOPIFY_DOMAIN'),
+            env('SHOPIFY_API_VERSION')
+        );
+
+        $response = $shopify->paginateProducts(['limit' => 10]);
+
+        $results = collect();
+
+        foreach ($response as $page) {
+            $results = $results->merge($page);
+        }
+
+        if ($results) {
+            return [
+                'results'  => $results,
+                'status' => true
+            ];
+        }
+
+        return false;
+    }
+}
